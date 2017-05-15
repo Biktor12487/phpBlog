8f41b5818653f504cecc35d18039f5a1ef9ef3a9
Victor <biktor12487@gmail.com>
2017-05-15 18:03:48 +0300

    making html/css tmpl

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>My Blog</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class="section navigation">
+		<div class="container">
+			<div class="navbar">
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">About Us</a></li>
+					<li><a href="">Contacts</a></li>
+					<li><a href="">FAQ</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<!-- begin header -->
+	<div class="section header">
+		<div class="container">
+			<h1>My Blog </h1>
+			<h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum iste tempora, error ex inventore incidunt porro animi. Dolorem cupiditate nam, quos rem, dolores, id mollitia consequuntur illum quo sunt similique!</h4>
+		</div>
+	</div>
+	<!-- endl header -->
+	<!-- begin Content -->
+	<div class="section content">
+		<div class="container">
+			<h2 class="title">News</h2>
+				<div class="node">
+					<h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, fugiat alias. Excepturi dolores nisi animi, sapiente deserunt totam eaque. Neque hic repellat, facere necessitatibus odit, molestiae ab odio itaque beatae.</h3>
+					<div class="textNode">
+						<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit quisquam aliquam cum asperiores, quis cumque dolor corporis libero ipsum earum commodi autem, maiores optio explicabo ducimus ad voluptas aperiam unde!</div>
+						<div>Eius amet tenetur consequuntur vero eveniet reprehenderit, ut illo nulla similique eligendi dicta earum reiciendis animi voluptas doloribus accusamus magni eos qui est, quod itaque! Eius, amet illum fuga. Quidem!</div>
+					</div>
+				</div>
+				<div class="node">
+					<h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo, deleniti quis, sint dolorem sed debitis in, possimus cumque numquam voluptatem eligendi ab, aspernatur doloremque magnam odio suscipit culpa dolores libero.</h3>
+					<div class="textNode">
+						<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus sint repudiandae eos, quia odit voluptatum, inventore porro sequi similique repellendus earum molestias perferendis provident ullam! Quibusdam expedita voluptates voluptas eius.</div>
+						<div>Optio blanditiis eum commodi quia eius tenetur deleniti vero voluptate iusto error, ullam ipsa non numquam sunt ab magni, consequatur quod sed enim qui eos facere hic. Laudantium, similique, officia.</div>
+					</div>
+				</div>
+				<div class="node">
+					<h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, vitae quidem ipsa deleniti recusandae ad eum molestiae cupiditate labore. In voluptatibus quo quasi nulla, dolore ipsum ipsam deleniti officia quidem.</h3>
+					<div class="textNode">
+						<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet qui et, accusamus rem nesciunt libero officiis error, ducimus saepe odio, dignissimos quam nobis quia alias suscipit enim quis a impedit.</div>
+						<div>Maiores voluptate, modi voluptatibus facilis accusamus laudantium unde mollitia illum, aliquam! Maiores nostrum iusto, animi officiis vel vitae commodi soluta error fugiat praesentium maxime, tempore, ipsam alias, cumque repellat excepturi.</div>
+					</div>
+				</div>
+				<div class="node">
+					<h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, fugiat alias. Excepturi dolores nisi animi, sapiente deserunt totam eaque. Neque hic repellat, facere necessitatibus odit, molestiae ab odio itaque beatae.</h3>
+					<div class="textNode">
+						<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit quisquam aliquam cum asperiores, quis cumque dolor corporis libero ipsum earum commodi autem, maiores optio explicabo ducimus ad voluptas aperiam unde!</div>
+						<div>Eius amet tenetur consequuntur vero eveniet reprehenderit, ut illo nulla similique eligendi dicta earum reiciendis animi voluptas doloribus accusamus magni eos qui est, quod itaque! Eius, amet illum fuga. Quidem!</div>
+					</div>
+				</div>
+				<div class="node">
+					<h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo, deleniti quis, sint dolorem sed debitis in, possimus cumque numquam voluptatem eligendi ab, aspernatur doloremque magnam odio suscipit culpa dolores libero.</h3>
+					<div class="textNode">
+						<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus sint repudiandae eos, quia odit voluptatum, inventore porro sequi similique repellendus earum molestias perferendis provident ullam! Quibusdam expedita voluptates voluptas eius.</div>
+						<div>Optio blanditiis eum commodi quia eius tenetur deleniti vero voluptate iusto error, ullam ipsa non numquam sunt ab magni, consequatur quod sed enim qui eos facere hic. Laudantium, similique, officia.</div>
+					</div>
+				</div>
+				<div class="node">
+					<h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, vitae quidem ipsa deleniti recusandae ad eum molestiae cupiditate labore. In voluptatibus quo quasi nulla, dolore ipsum ipsam deleniti officia quidem.</h3>
+					<div class="textNode">
+						<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet qui et, accusamus rem nesciunt libero officiis error, ducimus saepe odio, dignissimos quam nobis quia alias suscipit enim quis a impedit.</div>
+						<div>Maiores voluptate, modi voluptatibus facilis accusamus laudantium unde mollitia illum, aliquam! Maiores nostrum iusto, animi officiis vel vitae commodi soluta error fugiat praesentium maxime, tempore, ipsam alias, cumque repellat excepturi.</div>
+					</div>
+				</div>
+				<div class="navNode">
+					<div class="div nodeLinks">
+						<a href="">1</a>
+						<a href="">2</a>
+						<a href="">3</a>
+						<a href="">4</a>
+					</div>
+				</div>
+		</div>
+	</div>
+	<!-- endl Contetn -->
+	<!-- begin footer -->
+	<div class="section footer">
+		<div class="container">
+			<div class="sector firstFooter">
+				<ul class="socilaLinks">
+					<li><a href="">Vkontacte</a></li>
+					<li><a href="">YouTube</a></li>
+					<li><a href="">FaceBook</a></li>
+				</ul>
+			</div>
+			<div class="sector twoFooter">
+			 	<div class="about">
+				  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit iure optio, voluptatibus asperiores nesciunt. Officiis numquam, ut error eaque excepturi aliquid fugit explicabo dicta molestiae, consectetur deserunt ipsum saepe quibusdam.
+			 	</div>
+			</div>
+			<div class="sector threeFooter">
+			</div>
+			<div class="copyright">
+				2017 | Victor None
+			</div>
+		</div>
+	</div>
+	<!-- endl footer -->
+</body>
+</html>
